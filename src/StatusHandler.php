a26cc55118cf364acd670925da3dac5cbff4b40b
Brian Clement <brian@bclement.com>
2019-01-21 15:01:33 -0600

    Build out status handler service.

--- src/StatusHandler.php
+++ src/StatusHandler.php
@@ -1,7 +1,8 @@
 <?php
 
 namespace Drupal\eventbrite_events;
-use Drupal\eventbrite_events\ApiInterface;
+
+use Drupal\eventbrite_events\Api;
 
 /**
  * Class StatusHandler.
@@ -11,14 +12,31 @@ class StatusHandler {
   /**
    * Drupal\eventbrite_events\ApiInterface definition.
    *
-   * @var \Drupal\eventbrite_events\ApiInterface
+   * @var \Drupal\eventbrite_events\Api
    */
   protected $eventbriteEventsApi;
+
   /**
    * Constructs a new StatusHandler object.
    */
-  public function __construct(ApiInterface $eventbrite_events_api) {
+  public function __construct(Api $eventbrite_events_api) {
     $this->eventbriteEventsApi = $eventbrite_events_api;
+  }
+
+  /**
+   * Check the status of the event.
+   *
+   * @param int $eventbrite_event_id
+   * @return string
+   */
+  public function check($eventbrite_event_id) {
+    // Get the status response for the given event ID
+    $results = $this->eventbriteEventsApi->connect('get', "events/$eventbrite_event_id/", [], []);
+
+    $result = reset($results);
+
+    // Return Eventbrite's status code.
+    return $result['status'];
   }
 
 }
