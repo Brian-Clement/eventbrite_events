6ac878d3081c163a7d3587edfadc0d1b19881eea
Brian Clement <brian@bclement.com>
2019-01-21 13:27:04 -0600

    Add config form to store OAuth token.

---
+++ src/Form/EventbriteSettings.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Drupal\eventbrite_events\Form;
+
+use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Class EventbriteSettings.
+ */
+class EventbriteSettings extends ConfigFormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames() {
+    return [
+      'eventbrite_events.eventbritesettings',
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'eventbrite_settings';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+    $config = $this->config('eventbrite_events.eventbritesettings');
+    $form['eventbrite_oauth_token'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('OAuth Token'),
+      '#description' => $this->t('Enter your Eventbrite API token'),
+      '#maxlength' => 64,
+      '#size' => 64,
+      '#default_value' => $config->get('eventbrite_oauth_token'),
+    ];
+    return parent::buildForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    parent::validateForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    parent::submitForm($form, $form_state);
+
+    $this->config('eventbrite_events.eventbritesettings')
+      ->set('eventbrite_oauth_token', $form_state->getValue('eventbrite_oauth_token'))
+      ->save();
+  }
+
+}
