5c8e763956abad2b04072a89f7ad791615b10d1a
Brian Clement <brian@bclement.com>
2019-01-21 14:54:26 -0600

    Build out API service

--- src/Api.php
+++ src/Api.php
@@ -1,8 +1,10 @@
 <?php
 
 namespace Drupal\eventbrite_events;
+
 use GuzzleHttp\ClientInterface;
-use Drupal\Core\Config\ConfigFactoryInterface;
+use GuzzleHttp\Exception\RequestException;
+use Drupal\Core\Config\ConfigFactory;
 
 /**
  * Class Api.
@@ -15,18 +17,87 @@ class Api implements ApiInterface {
    * @var \GuzzleHttp\ClientInterface
    */
   protected $httpClient;
+
   /**
-   * Drupal\Core\Config\ConfigFactoryInterface definition.
+   * Drupal\Core\Config\ConfigFactory definition.
    *
-   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   * @var \Drupal\Core\Config\ConfigFactory;
    */
-  protected $configFactory;
+  protected $config;
+
+  /**
+   * Eventbrite API Token.
+   *
+   * @var string
+   */
+  protected $token;
+
+  /**
+   * Eventbrite Base URI.
+   *
+   * @var string
+   */
+  protected $base_uri;
+
   /**
    * Constructs a new Api object.
    */
-  public function __construct(ClientInterface $http_client, ConfigFactoryInterface $config_factory) {
+  public function __construct(ClientInterface $http_client, ConfigFactory $config_factory) {
     $this->httpClient = $http_client;
-    $this->configFactory = $config_factory;
+    $this->config = $config_factory->get('eventbrite_events.settings');
+    $this->token = $this->config->get('oauth_token');
+    $this->base_uri = 'https://www.eventbriteapi.com/v3/';
+  }
+
+  /**
+   * { @inheritdoc }
+   */
+  public function connect($method, $endpoint, $query, $body, $page = 1) {
+    try {
+      $response = $this->httpClient->{$method}(
+        $this->base_uri . $endpoint,
+        $this->buildOptions($query, $body)
+      );
+    }
+    catch (RequestException $exception) {
+      $msg = t('Eventbrite "%error"', ['%error' => $exception->getMessage()]);
+      \Drupal::messenger()->addMessage($msg, self::TYPE_ERROR, TRUE);
+      \Drupal::logger('eventbrite_events.api')->error('Eventbrite "%error"', ['%error' => $exception->getMessage()]);
+      return FALSE;
+    }
+
+    $pages = [];
+
+    $results = json_decode($response->getBody(), TRUE);
+
+    $pages[$page] = $results;
+
+    if (isset($results['pagination']) && $results['pagination']['has_more_items'] && $results['pagination']['page_number'] < $results['pagination']['page_count']) {
+      $query = [
+        'continuation' => $results['pagination']['continuation']
+      ];
+
+      $pages += $this->connect($method, $endpoint, $query, $body, $page + 1);
+    }
+
+    return $pages;
+  }
+
+  /**
+   * Build options for the client.
+   */
+  private function buildOptions($query, $body) {
+    $options = [];
+    $options['headers'] = [
+      'Authorization' => 'Bearer ' . $this->token,
+    ];
+    if ($body) {
+      $options['body'] = $body;
+    }
+    if ($query) {
+      $options['query'] = $query;
+    }
+    return $options;
   }
 
 }
--- src/ApiInterface.php
+++ src/ApiInterface.php
@@ -6,6 +6,19 @@ namespace Drupal\eventbrite_events;
  * Interface ApiInterface.
  */
 interface ApiInterface {
-
-
+  /**
+   * Utilizes Drupal's httpClient to connect to the Eventbrite API
+   *
+   * @param string $method
+   *   get, post, patch, delete, etc. See Guzzle documentation.
+   * @param string $endpoint
+   *   The Eventbrite API endpoint (ex. users/me/owned_events)
+   * @param array $query
+   *   Query string parameters the endpoint allows (ex. ['per_page' => 50]
+   * @param array $body (converted to JSON)
+   *   Utilized for some endpoints
+   * @return object
+   *   \GuzzleHttp\Psr7\Response body
+   */
+  public function connect($method, $endpoint, $query, $body);
 }
